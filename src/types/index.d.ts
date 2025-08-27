daa078189567102c7eba5c395bba355f9e7442b0
Fortune Teller <fortuneteller@example.com>
2025-08-27 13:21:50 +0900

    fix: apply specific naver type to global declaration

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,8 +1,12 @@
 // 파일 경로: src/types/index.d.ts
 
-// naver 객체가 window 전역 객체에 존재한다고 타입스크립트에게 알려줍니다.
+// '@types/navermaps' 패키지에서 naver 객체의 실제 타입을 가져옵니다.
+import type { naver } from '@types/navermaps';
+
+// window 전역 객체에 naver 객체가 존재하며,
+// 그 타입은 '@types/navermaps'에서 가져온 naver 타입이라고 명시합니다.
 declare global {
   interface Window {
-    naver: any;
+    naver: naver;
   }
 }
