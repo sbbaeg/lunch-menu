56b13a82e0bb2f885b541a3d3af138bd20a5d1c4
Fortune Teller <fortuneteller@example.com>
2025-08-28 14:49:29 +0900

    fix: 빌드 오류 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import '@/types/index.d';
+
 
 interface NaverRestaurantItem {
   title: string;
